4f6e2fbfc042595b436c506f9ab8805020713f40
Sunday Obinna Oruwhone <sundayamosobi@gmail.com>
2022-06-07 06:55:46 +0100

    <Main /> housekeeping

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,13 +6,13 @@ export default function Main() {
             <h1 className="title text-center">WEBSITE COPIER ONLINE&nbsp;<span className="free">FREE</span></h1>
             <section>
                 <div className="row text-center">
-                    <div className="col-sm-3 col-md-3 left">
+                    <div className="col-xs-4 col-sm-3 col-md-3 left">
                         <div className="stats">
                             <h1><RandomStats /></h1>
                             <small className='text-center'>Total Websites Copied</small>
                         </div>
                     </div>
-                    <div className="col-sm-6 col-md-6 center">
+                    <div className="col-xs-8 col-sm-6 col-md-6 center">
                         <form className="form">
                             <div className="row input-area">
                                 <div className="col-sm-10 col-md-10">
@@ -34,7 +34,7 @@ export default function Main() {
                             </div>
                         </form>
                     </div>
-                    <div className="col-sm-3 col-md-3 right"></div>
+                    <div className="col-xs-0 col-sm-3 col-md-3 right"></div>
                 </div>
             </section>
         </main>
